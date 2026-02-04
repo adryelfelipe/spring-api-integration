62e88df6bca6453a37802ad91551e05601f04ebd
ADRYEL FELIPE SAPELLI DE SOUZA <adryel_souza@estudante.sesisenai.org.br>
2026-02-04 19:33:56 -0300

    feat(client): adiciona método de get em ClientRepository

--- productsApi/src/main/java/client/infra/repository/ClientRepository.java
+++ productsApi/src/main/java/client/infra/repository/ClientRepository.java
@@ -1,7 +1,9 @@
 package client.infra.repository;
 
+import client.dto.get.GetClientResponse;
 import client.model.Client;
 
 public interface ClientRepository {
     void save(Client client);
+    GetClientResponse get(Client client);
 }
