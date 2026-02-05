5f4ca05e58ad14d381ecbb0eeb3fd9622aec247f
Adryel Felipe <adryxd003@gmail.com>
2026-02-05 11:10:31 -0300

    feat(client): adiciona validações extras em GetClientResponse e CreateClientRequest

--- productsApi/src/main/java/client/dto/create/CreateClientRequest.java
+++ productsApi/src/main/java/client/dto/create/CreateClientRequest.java
@@ -6,13 +6,15 @@ import jakarta.validation.constraints.Size;
 
 public record CreateClientRequest(
         @NotBlank(message = "O nome é obrigatório")
+        @Size(min = 2, max = 199, message = "O nome deve ter entre 2 e 199 caracteres")
         String name,
 
         @NotBlank(message = "A senha é obrigatória")
         @Size(min = 8, message = "A senha deve ter no mínimo 8 caracteres")
         String password,
 
-        @NotBlank(message = "O email é obrigatório")
-        @Email(message = "O email está inválido")
+        @NotBlank(message = "O e-mail é obrigatório")
+        @Email(message = "Formato de e-mail inválido")
+        @Size(max = 254, message = "O e-mail não pode exceder 254 caracteres")
         String email
 ) {}
--- productsApi/src/main/java/client/dto/get/GetClientResponse.java
+++ productsApi/src/main/java/client/dto/get/GetClientResponse.java
@@ -3,13 +3,16 @@ package client.dto.get;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 
 public record GetClientResponse(
         @NotBlank(message = "O nome é obrigatório")
+        @Size(min = 2, max = 199, message = "O nome deve ter entre 2 e 199 caracteres")
         String name,
 
-        @NotBlank(message = "O email é obrigatório")
-        @Email(message = "O email está inválido")
+        @NotBlank(message = "O e-mail é obrigatório")
+        @Email(message = "Formato de e-mail inválido")
+        @Size(max = 254, message = "O e-mail não pode exceder 254 caracteres")
         String email,
 
         @PositiveOrZero(message = "O id do cliente deve ser maior ou igual a 0")
